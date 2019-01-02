f4617b89e032db57973c38b4323562841ba2d5c1
Koichi Nakamura <koichi@idein.jp>
2019-01-03 08:30:02 +0900

    Rename _float_ptr to _fp

--- lsdslam/lib.py
+++ lsdslam/lib.py
@@ -3,45 +3,45 @@ from ctypes import cdll, c_float, POINTER
 
 lib = cdll.LoadLibrary('src/liblsdslam.so')
 
-def _float_ptr(arr):
+def _fp(arr):
     return arr.ctypes.data_as(POINTER(c_float))
 
 def mul3x3(A, B):
     C = np.zeros_like(A)
-    lib.mul3x3(_float_ptr(C), _float_ptr(A), _float_ptr(B))
+    lib.mul3x3(_fp(C), _fp(A), _fp(B))
     return C
 
 lib.det3x3.restype = c_float
 def det3x3(A):
-    return lib.det3x3(_float_ptr(A))
+    return lib.det3x3(_fp(A))
 
 def inv3x3(A):
     B = np.zeros_like(A)
-    lib.inv3x3(_float_ptr(B), _float_ptr(A))
+    lib.inv3x3(_fp(B), _fp(A))
     return B
 
 def affine3d(A, b, x):
     y = np.zeros_like(x)
-    lib.affine3d(_float_ptr(y), _float_ptr(A), _float_ptr(b), _float_ptr(x))
+    lib.affine3d(_fp(y), _fp(A), _fp(b), _fp(x))
     return y
 
 def R(n, theta):
     y = np.zeros((3, 3), dtype=np.float32)
-    lib.compute_R(_float_ptr(y), _float_ptr(n), c_float(theta))
+    lib.compute_R(_fp(y), _fp(n), c_float(theta))
     return y
 
 def R_theta(n, theta):
     y = np.zeros((3, 3), dtype=np.float32)
-    lib.compute_R_theta(_float_ptr(y), _float_ptr(n), c_float(theta))
+    lib.compute_R_theta(_fp(y), _fp(n), c_float(theta))
     return y
 
 def R_n(n, theta):
     y = np.zeros((3, 3, 3), dtype=np.float32)
-    lib.compute_R_n(_float_ptr(y), _float_ptr(n), c_float(theta))
+    lib.compute_R_n(_fp(y), _fp(n), c_float(theta))
     return y
 
 def precompute_T(rho, n, theta, t):
     A = np.zeros((3, 3), dtype=np.float32)
     b = np.zeros(3, dtype=np.float32)
-    lib.precompute_T(_float_ptr(A), _float_ptr(b), c_float(rho), _float_ptr(n), c_float(theta), _float_ptr(t))
+    lib.precompute_T(_fp(A), _fp(b), c_float(rho), _fp(n), c_float(theta), _fp(t))
     return A, b
