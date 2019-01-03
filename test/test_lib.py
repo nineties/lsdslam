129d4ca2728f498abd2fb5b8bfe50c7dd4bd6fb9
Koichi Nakamura <koichi@idein.jp>
2019-01-03 23:55:42 +0900

    separate utilities from test_lib.py

--- test/test_lib.py
+++ test/test_lib.py
@@ -6,29 +6,7 @@ from scipy.ndimage import sobel
 from PIL import Image
 
 import lib as L
-
-# Utilities
-
-def assert_allclose(x, y, rtol=1e-3, atol=0):
-    np.testing.assert_allclose(x, y, rtol=rtol, atol=atol)
-
-def repeat(n):
-    def decorate(f):
-        def newfunc(*args, **kw):
-            for i in range(n):
-                f(*args, **kw)
-        return make_decorator(f)(newfunc)
-    return decorate
-
-def random_vec(n):
-    return np.random.randn(n).astype(np.float32)
-
-def random_norm(n):
-    v = random_vec(n)
-    return v/np.sqrt(v.dot(v))
-
-def read_image(path, dtype=np.float32):
-    return np.asarray(Image.open(path).convert('P').resize(L.SIZE), dtype=dtype)
+from util import *
 
 def test_mul3x3():
     A = np.random.randn(3, 3).astype(np.float32)
---
+++ test/util.py
@@ -0,0 +1,27 @@
+import numpy as np
+from PIL import Image
+import lib as L
+
+# Utilities
+
+def assert_allclose(x, y, rtol=1e-3, atol=0):
+    np.testing.assert_allclose(x, y, rtol=rtol, atol=atol)
+
+def repeat(n):
+    def decorate(f):
+        def newfunc(*args, **kw):
+            for i in range(n):
+                f(*args, **kw)
+        return make_decorator(f)(newfunc)
+    return decorate
+
+def random_vec(n):
+    return np.random.randn(n).astype(np.float32)
+
+def random_norm(n):
+    v = random_vec(n)
+    return v/np.sqrt(v.dot(v))
+
+def read_image(path, dtype=np.float32):
+    return np.asarray(Image.open(path).convert('P').resize(L.SIZE), dtype=dtype)
+
